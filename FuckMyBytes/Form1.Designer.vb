9a91451dea14493f017b6b79493329fe4cfe9b7a
BalazsManus <balazsmanus@github.com>
2023-11-07 14:22:08 +0100

    upload

--- FuckMyBytes/Form1.Designer.vb
+++ FuckMyBytes/Form1.Designer.vb
@@ -22,10 +22,87 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Label1 = New Label()
+        RichTextBox1 = New RichTextBox()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(47, 12)
+        TextBox1.Name = "TextBox1"
+        TextBox1.PlaceholderText = "String"
+        TextBox1.Size = New Size(366, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(47, 59)
+        TextBox2.Name = "TextBox2"
+        TextBox2.PlaceholderText = "Password"
+        TextBox2.Size = New Size(366, 23)
+        TextBox2.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(486, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(132, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "Encrypt"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(486, 59)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(132, 23)
+        Button2.TabIndex = 3
+        Button2.Text = "Decrypt"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(47, 105)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(43, 15)
+        Label1.TabIndex = 4
+        Label1.Text = "output"
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Location = New Point(47, 133)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(710, 293)
+        RichTextBox1.TabIndex = 5
+        RichTextBox1.Text = ""
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(RichTextBox1)
+        Controls.Add(Label1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
 
 End Class
--- FuckMyBytes/Form1.vb
+++ FuckMyBytes/Form1.vb
@@ -1,3 +1,10 @@
-﻿Public Class Form1
+﻿Imports FuckMyBytes.EncryptionProviders
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        RichTextBox1.Text = EncryptionProviders.AES_Encrypt(TextBox1.Text, TextBox2.Text)
+    End Sub
 
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        RichTextBox1.Text = EncryptionProviders.AES_Decrypt(TextBox1.Text, TextBox2.Text)
+    End Sub
 End Class
