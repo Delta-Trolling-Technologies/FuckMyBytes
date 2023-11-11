633b51ab8ec282fb0e10a3868b1e22c4fd5e3a83
BalazsManus <balazsmanus@github.com>
2023-11-11 17:48:38 +0100

    add a splash, do some code cleanup

---
+++ FuckMyBytes/Splash.Designer.vb
@@ -0,0 +1,136 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Splash
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+    Friend WithEvents MainLayoutPanel As System.Windows.Forms.TableLayoutPanel
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Splash))
+        MainLayoutPanel = New TableLayoutPanel()
+        DetailsLayoutPanel = New TableLayoutPanel()
+        Version = New Label()
+        Copyright = New Label()
+        ApplicationTitle = New Label()
+        MainLayoutPanel.SuspendLayout()
+        DetailsLayoutPanel.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MainLayoutPanel
+        ' 
+        MainLayoutPanel.BackColor = Color.Transparent
+        MainLayoutPanel.BackgroundImageLayout = ImageLayout.Stretch
+        MainLayoutPanel.ColumnCount = 1
+        MainLayoutPanel.ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 275F))
+        MainLayoutPanel.ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 20F))
+        MainLayoutPanel.Controls.Add(DetailsLayoutPanel, 1, 1)
+        MainLayoutPanel.Controls.Add(ApplicationTitle, 0, 0)
+        MainLayoutPanel.Dock = DockStyle.Fill
+        MainLayoutPanel.Location = New Point(0, 0)
+        MainLayoutPanel.Name = "MainLayoutPanel"
+        MainLayoutPanel.RowCount = 2
+        MainLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Absolute, 218F))
+        MainLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Absolute, 38F))
+        MainLayoutPanel.Size = New Size(496, 303)
+        MainLayoutPanel.TabIndex = 0
+        ' 
+        ' DetailsLayoutPanel
+        ' 
+        DetailsLayoutPanel.BackColor = Color.Transparent
+        DetailsLayoutPanel.ColumnCount = 1
+        DetailsLayoutPanel.ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 142F))
+        DetailsLayoutPanel.ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 142F))
+        DetailsLayoutPanel.Controls.Add(Version, 0, 0)
+        DetailsLayoutPanel.Controls.Add(Copyright, 0, 1)
+        DetailsLayoutPanel.Dock = DockStyle.Bottom
+        DetailsLayoutPanel.ForeColor = Color.FromArgb(CByte(224), CByte(224), CByte(224))
+        DetailsLayoutPanel.Location = New Point(3, 221)
+        DetailsLayoutPanel.Name = "DetailsLayoutPanel"
+        DetailsLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 33F))
+        DetailsLayoutPanel.RowStyles.Add(New RowStyle(SizeType.Percent, 33F))
+        DetailsLayoutPanel.Size = New Size(490, 79)
+        DetailsLayoutPanel.TabIndex = 1
+        ' 
+        ' Version
+        ' 
+        Version.BackColor = Color.Transparent
+        Version.Dock = DockStyle.Fill
+        Version.Font = New Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        Version.Location = New Point(3, 0)
+        Version.Name = "Version"
+        Version.Size = New Size(484, 39)
+        Version.TabIndex = 1
+        Version.Text = "Version {0}.{1:00}"
+        Version.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Copyright
+        ' 
+        Copyright.BackColor = Color.Transparent
+        Copyright.Dock = DockStyle.Fill
+        Copyright.Font = New Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point)
+        Copyright.Location = New Point(3, 39)
+        Copyright.Name = "Copyright"
+        Copyright.Size = New Size(484, 40)
+        Copyright.TabIndex = 2
+        Copyright.Text = "Copyright"
+        Copyright.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' ApplicationTitle
+        ' 
+        ApplicationTitle.BackColor = Color.Transparent
+        ApplicationTitle.Dock = DockStyle.Fill
+        ApplicationTitle.Font = New Font("Microsoft Sans Serif", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        ApplicationTitle.ForeColor = Color.FromArgb(CByte(224), CByte(224), CByte(224))
+        ApplicationTitle.Image = CType(resources.GetObject("ApplicationTitle.Image"), Image)
+        ApplicationTitle.ImageAlign = ContentAlignment.TopRight
+        ApplicationTitle.Location = New Point(3, 0)
+        ApplicationTitle.Name = "ApplicationTitle"
+        ApplicationTitle.Size = New Size(490, 218)
+        ApplicationTitle.TabIndex = 0
+        ApplicationTitle.Text = "Application Title"
+        ApplicationTitle.TextAlign = ContentAlignment.BottomCenter
+        ' 
+        ' Splash
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Black
+        BackgroundImageLayout = ImageLayout.None
+        ClientSize = New Size(496, 303)
+        ControlBox = False
+        Controls.Add(MainLayoutPanel)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        MinimizeBox = False
+        Name = "Splash"
+        ShowInTaskbar = False
+        StartPosition = FormStartPosition.CenterScreen
+        MainLayoutPanel.ResumeLayout(False)
+        DetailsLayoutPanel.ResumeLayout(False)
+        ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DetailsLayoutPanel As TableLayoutPanel
+    Friend WithEvents Version As Label
+    Friend WithEvents Copyright As Label
+    Friend WithEvents ApplicationTitle As Label
+
+End Class
---
+++ FuckMyBytes/Splash.vb
@@ -0,0 +1,12 @@
+﻿Public NotInheritable Class Splash
+    Private Sub Splash_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        If My.Application.Info.Title <> "" Then
+            ApplicationTitle.Text = My.Application.Info.Title
+        Else
+            ApplicationTitle.Text = IO.Path.GetFileNameWithoutExtension(My.Application.Info.AssemblyName)
+        End If
+        Version.Text = String.Format(Version.Text, My.Application.Info.Version.Major, My.Application.Info.Version.Minor)
+        Copyright.Text = My.Application.Info.Copyright
+    End Sub
+
+End Class
